7ad7fe414e3033a73fa4ff33452d4b1ae4dfc26b
GB <voducthang16@gmail.com>
2022-08-01 20:13:30 +0700

    add Login Screen
    01.08.2022 - 8:15 PM

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen, LoginScreen } from './src/screen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let initRoute = 'Home';
+    initRoute = 'Login';
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName={initRoute}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
---
+++ src/screen/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,101 @@
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    SafeAreaView,
+    TextInput,
+    TouchableOpacity,
+    Platform,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import images from '../../assets/images';
+function Login({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="dark" />
+            <KeyboardAwareScrollView contentContainerStyle={styles.wrapper}>
+                <ScrollView contentContainerStyle={styles.form} keyboardShouldPersistTaps="handled">
+                    <Image style={styles.image} source={images.logo} />
+                    <TextInput
+                        selectionColor={'black'}
+                        style={styles.input}
+                        placeholder="Phone Number"
+                        keyboardType="numeric"
+                    />
+                    <TextInput style={styles.input} placeholder="Password" selectionColor={'black'} />
+                    <Text style={styles.fp}>Forgot Password ?</Text>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.btnText}>Login</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.register}>
+                        Don't have an account!{' '}
+                        <Text style={styles.textColor} onPress={() => navigation.navigate('Home')}>
+                            Register
+                        </Text>
+                    </Text>
+                </ScrollView>
+            </KeyboardAwareScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fafafa',
+    },
+    wrapper: {
+        flex: 1,
+        width: '80%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    image: {
+        width: 160,
+        height: 160,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        margin: 8,
+        borderRadius: 8,
+        backgroundColor: '#f1f1f1',
+        padding: 16,
+    },
+    btn: {
+        height: 50,
+        margin: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        backgroundColor: '#ee0f37',
+    },
+    btnText: {
+        fontSize: 16,
+        color: '#fff',
+    },
+    fp: {
+        margin: 8,
+        textAlign: 'right',
+        color: '#ee0f37',
+    },
+    register: {
+        textAlign: 'center',
+        marginTop: 36,
+    },
+    textColor: {
+        color: '#ee0f37',
+    },
+});
+
+export default Login;
